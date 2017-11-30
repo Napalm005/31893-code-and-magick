7852f77327281573e7c2832a5199054401a3112b
aleksandr.frolov <aleksandr.frolov@sravni.ru>
2017-11-30 18:20:44 +0300

    task canvas  complited

--- js/stat.js
+++ js/stat.js
@@ -10,5 +10,43 @@ window.renderStatistics = (function (ctx, names, times) {
   ctx.font = '16px PT Mono';
   ctx.textBaseline = 'hanging';
   ctx.fillText('Ура вы победили!', 120, 30);
-  ctx.fillText('Список результатов:', 120, 55);
+  ctx.fillText('Список результатов:', 120, 52);
+
+  var max = getMaxElement(times);
+
+  var histogramHeight = 150;
+  var step = histogramHeight / (max - 0);
+
+  var barWidth = 40;
+  var indent = 50;
+  var initialX = 140;
+  var initialY = 100;
+  var histogramMargin = 5;
+  var scoreOffset = 18;
+
+  for (var i = 0; i < times.length; i++) {
+    ctx.fillText(parseInt(times[i], 10), initialX + indent * i + barWidth * i, initialY + histogramHeight - times[i] * step - scoreOffset);
+    ctx.fillText(names[i], initialX + indent * i + barWidth * i, initialY + histogramHeight + histogramMargin);
+  }
+
+  for (var index = 0; index < times.length; index++) {
+    if (names[i] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      ctx.fillStyle = 'rgba(0, 0, 255, ' + Math.random() + ')';
+    }
+    ctx.fillRect(initialX + indent * i + barWidth * i, initialY + histogramHeight - times[i] * step, barWidth, times[i] * step);
+  }
 });
+
+function getMaxElement(arr) {
+  var max = -1;
+
+  for (var i = 0; i < arr.length; i++) {
+    var time = arr[i];
+    if (time > max) {
+      max = time;
+    }
+  }
+  return max;
+}
