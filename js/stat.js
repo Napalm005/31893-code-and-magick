93c0b49c76dc3adf99ae79235b081106314a3b85
Napalm005 <Napalm_08@mail.ru>
2017-11-30 10:41:18 +0300

    create remote branch module2-task1

---
+++ js/stat.js
@@ -0,0 +1,14 @@
+'use strict';
+
+window.renderStatistics = (function (ctx, names, times) {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.fillRect(110, 20, 420, 270);
+  ctx.fillStyle = '#fff';
+  ctx.fillRect(100, 10, 420, 270);
+
+  ctx.fillStyle = 'black';
+  ctx.font = '16px PT Mono';
+  ctx.textBaseline = 'hanging';
+  ctx.fillText('Ура вы победили!', 120, 30);
+  ctx.fillText('Список результатов:', 120, 55);
+});
