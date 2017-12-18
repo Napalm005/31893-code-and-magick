8dcfeb6ccbbfb23a917335bd0adfe932a96ceb09
aleksandr.frolov <aleksandr.frolov@sravni.ru>
2017-12-18 15:51:59 +0300

    module6-task1 done

--- js/wizardCustomisation.js
+++ js/wizardCustomisation.js
@@ -6,14 +6,15 @@
   var wizardEyes = wizard.querySelector('.wizard-eyes');
   var fireball = document.querySelector('.setup-fireball-wrap');
 
-  wizardCoat.addEventListener('click', function (evt) {
-    evt.target.style.fill = window.vars.WIZARD_COAT_COLOR[window.util.getRandomArbitary(0, window.vars.WIZARD_COAT_COLOR.length)];
-  });
-  wizardEyes.addEventListener('click', function (evt) {
-    evt.target.style.fill = window.vars.WIZARD_EYES_COLOR[window.util.getRandomArbitary(0, window.vars.WIZARD_EYES_COLOR.length)];
-  });
-  fireball.addEventListener('click', function (evt) {
-    evt.target.style.backgroundColor = window.vars.WIZARD_FIREBALL_COLOR[window.util.getRandomArbitary(0, window.vars.WIZARD_FIREBALL_COLOR.length)];
-  });
+  window.colorizeElement(wizardCoat, window.vars.WIZARD_COAT_COLOR, fillElement);
+  window.colorizeElement(wizardEyes, window.vars.WIZARD_EYES_COLOR, fillElement);
+  window.colorizeElement(fireball, window.vars.WIZARD_FIREBALL_COLOR, changeElementBackground);
+
+  function fillElement(element, color) {
+    element.style.fill = color;
+  };
+  function changeElementBackground(element, color) {
+    element.style.backgroundColor = color;
+  };
 })();
 
